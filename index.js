10a85071ead9663070fe8976d08db8e17f6d80e7
Clayton Carlson <claytonrc97@gmail.com>
2023-01-30 18:54:09 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.ceil(Math.random()* 100);
+
+const num3 = 64;
+const num4 = 12;
+const mod = num3 % num4;
+
+const numberSet = [3, 8, 12, 15, 18, 20];
+
+const max = Math.max(...numberSet);
